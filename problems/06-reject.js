4e1346cf77046f27c828acdd6be4a480a2fee115
Pristine <prisvsyo@gmail.com>
2024-02-12 17:47:55 -0800

    completed problem 06 reject

--- problems/06-reject.js
+++ problems/06-reject.js
@@ -22,7 +22,14 @@ console.log(reject(['breadth', 'GRAPH', 'depth', 'height'], hasA)); // [ 'depth'
 *******************************************************************************/
 
 function reject(array, cb) {
-  // Your code here 
+  // Your code here
+  let result = [];
+  for (let el of array) {
+    if (!cb(el)) {
+      result.push(el);
+    }
+  }
+  return result;
 }
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
